192ff6bd9bec0b232c162e941269b650f151af00
jamie brim <jamie@airbnb.com>
2017-02-19 18:34:05 -0800

    [terraform] add support for vpc flow logs

---
+++ terraform/modules/tf_stream_alert_flow_logs/variables.tf
@@ -0,0 +1,16 @@
+variable "destination_stream_arn" { }
+variable "region" { }
+variable "flow_log_group_name" { }
+
+variable "targets" {
+    type = "map"
+    default = {
+        "vpcs" = []
+        "subnets" = []  
+        "enis" = []
+    }
+}
+
+variable "flow_log_filter" {
+    default = "[version, account, eni, source, destination, srcport, destport, protocol, packets, bytes, windowstart, windowend, action, flowlogstatus]"
+}
