c841cea16724bd43e11fb03935b76f560b9c96a6
Jack Naglieri <jack.naglieri@airbnb.com>
2017-03-09 15:09:57 -0800

    [cli][testing] integration testing helpers, and new rule helpers
    * add the capability to add integration testing helpers
    * update sample configuration, rules, and tests
    * add netaddr to the default variables.json third-party libraries
    * add in_network, and valid_ip base rule helpers

--- rules/helpers/base.py
+++ rules/helpers/base.py
@@ -14,9 +14,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 '''
 
+import logging
 import time
 
 from fnmatch import fnmatch
+from netaddr import IPAddress, IPNetwork
+from netaddr.core import AddrFormatError
+
+logging.basicConfig()
+LOGGER = logging.getLogger('StreamAlert')
 
 def in_set(data, whitelist):
     """Checks if data exists in any elements of a whitelist.
@@ -30,7 +36,7 @@ def in_set(data, whitelist):
     """
     return any(fnmatch(data, x) for x in whitelist)
 
-def last_hour(unixtime):
+def last_hour(unixtime, hours=1):
     """Check if a given epochtime is within the last hour.
 
     Args:
@@ -39,8 +45,44 @@ def last_hour(unixtime):
     Returns:
         True/False
     """
+    seconds = hours * 3600
     # sometimes bash histories do not contain the `time` column
     if unixtime:
-        return int(time.time()) - int(unixtime) <= 3600
+        return int(time.time()) - int(unixtime) <= seconds
     else:
         return False
+
+def valid_ip(ip_address):
+    """Verify that a ip_address string is valid
+
+    Args:
+        ip_address: A string to cast into an IPAddress class
+
+    Returns:
+        IPAddress object or None if an invalid address
+    """
+    try:
+        valid_ip = IPAddress(ip_address)
+    except AddrFormatError:
+        return False
+    return True
+
+def in_network(ip_address, cidrs):
+    """Check that an ip_address is within a set of CIDRs
+
+    Args:
+        ip_address: netaddr IPAddress object
+        cidrs: a set of string CIDRs
+
+    Returns:
+        Boolean representing if the given IP is within any CIDRs
+    """
+    for cidr in cidrs:
+        try:
+            network = IPNetwork(cidr)
+        except AddrFormatError:
+            LOGGER.error('Invalid IP Network: %s', cidr)
+            continue
+        if ip_address in network:
+            return True
+    return False
