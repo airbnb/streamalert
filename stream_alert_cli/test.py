1438416b6cee1c5a8db0344566800ff7810e13e7
Ryan Deivert <ryan.deivert@airbnb.com>
2017-03-16 13:28:12 -0700

    [testing] updating cli testing to include s3 as a service Slight refactor to allow for a more generic way of testing various services. Updating `sources.json` to include various source log types for s3. `requirements.txt` now includes `moto==0.4.31`, as this is now used for boto3 mocking. Updated integration rule files to include s3 service examples.

--- stream_alert_cli/test.py
+++ stream_alert_cli/test.py
@@ -18,9 +18,12 @@ import base64
 import json
 import logging
 import os
+import random
 import re
 import time
 
+import boto3
+from moto import mock_s3
 from stream_alert.handler import StreamAlert
 # import all rules loaded from the main handler
 import main
@@ -28,6 +31,8 @@ import main
 LOGGER_SA = logging.getLogger('StreamAlert')
 LOGGER_CLI = logging.getLogger('StreamAlertCLI')
 LOGGER_CLI.setLevel(logging.INFO)
+
+BOTO_MOCKER = mock_s3()
 
 DIR_RULES = 'test/integration/rules'
 DIR_TEMPLATES = 'test/integration/templates'
@@ -41,7 +46,7 @@ def report_output(cols, force_exit):
         cols: A list of columns to print (test description, pass|fail)
         force_exit: Boolean to break exectuion of integration testing
     """
-    print '\ttest: {: <40} {: >25}'.format(*cols)
+    print '\t{}\ttest ({}): {}'.format(*cols)
     if force_exit:
         os._exit(1)
 
@@ -55,26 +60,24 @@ def test_rule(rule_name, test_file_contents):
     print '\n{}'.format(rule_name)
 
     for record in test_file_contents['records']:
-        context = None
-        event = {'Records': []}
-        event['Records'].append(record['kinesis_data'])
-        if record['trigger']:
-            expected_alerts = 1
-        else:
-            expected_alerts = 0
+        service = record['service']
+        service_record_key = '{}_record'.format(service)
+        event = {'Records': [record[service_record_key]]}
 
-        alerts = StreamAlert(return_alerts=True).run(event, context)
+        expected_alert_count = (0, 1)[record['trigger']]
+
+        alerts = StreamAlert(return_alerts=True).run(event, None)
         # we only want alerts for the specific rule passed in
-        matched_alerts = [x for x in alerts if x['rule_name'] == rule_name]
+        matched_alert_count = len([x for x in alerts if x['rule_name'] == rule_name])
 
-        if len(matched_alerts) == expected_alerts:
+        if matched_alert_count == expected_alert_count:
             result = '{}[Pass]{}'.format(COLOR_GREEN, COLOR_RESET)
             force_exit = False
         else:
             result = '{}[Fail]{}'.format(COLOR_RED, COLOR_RESET)
             force_exit = True
 
-        report_output([record['description'], result], force_exit)
+        report_output([result, service, record['description']], force_exit)
 
 def format_record(test_record):
     """Create a properly formatted Kinesis, S3, or SNS record.
@@ -90,7 +93,7 @@ def format_record(test_record):
             service - which aws service originated the data
 
     Returns:
-        populated dict in the format of the specific service.
+        dict in the format of the specific service
     """
     service = test_record['service']
     source = test_record['source']
@@ -104,27 +107,33 @@ def format_record(test_record):
         LOGGER_CLI.info('Invalid data type: %s', type(test_record['data']))
         return
 
+    # Get the template file for this particular service
+    template_path = os.path.join(DIR_TEMPLATES, '{}.json'.format(service))
+    with open(template_path, 'r') as service_template:
+        try:
+            template = json.load(service_template)
+        except ValueError as err:
+            LOGGER_CLI.error('Error loading %s.json: %s', service, err)
+            return
     if service == 's3':
-        pass
+        # Set the S3 object key to a random value for testing
+        test_record['key'] = ('{:032X}'.format(random.randrange(16**32)))
+        template['s3']['object']['key'] = test_record['key']
+        template['s3']['bucket']['arn'] = 'arn:aws:s3:::{}'.format(source)
+        template['s3']['bucket']['name'] = source
 
+        # Create the mocked s3 object in the designated bucket with the random key
+        put_mocked_s3_object(source, test_record['key'], data)
     elif service == 'kinesis':
-        kinesis_path = os.path.join(DIR_TEMPLATES, 'kinesis.json')
-        with open(kinesis_path, 'r') as kinesis_template:
-            try:
-                template = json.load(kinesis_template)
-            except ValueError as err:
-                LOGGER_CLI.error('Error loading kinesis.json: %s', err)
-                return
-
         template['kinesis']['data'] = base64.b64encode(data)
         template['eventSourceARN'] = 'arn:aws:kinesis:us-east-1:111222333:stream/{}'.format(source)
-        return template
-
     elif service == 'sns':
+        # TODO implement sns testing
         pass
-
     else:
         LOGGER_CLI.info('Invalid service %s', service)
+
+    return template
 
 def check_keys(test_record):
     """Check the test_record contains the required keys
@@ -133,7 +142,7 @@ def check_keys(test_record):
         test_record: Test record metadata dict
 
     Returns:
-        Boolean result of key set comparison
+        boolean result of key set comparison
     """
     req_keys = {
         'data',
@@ -175,8 +184,11 @@ def apply_helpers(test_record):
 
     find_and_apply_helpers(test_record)
 
-def test_kinesis_alert_rules():
-    """Integration test the 'Alert' Lambda function with Kinesis records"""
+def test_alert_rules():
+    """Integration test the 'Alert' Lambda function with various record types"""
+    # Start the mock_s3 instance here so we can test with mocked objects project-wide
+    BOTO_MOCKER.start()
+
     for root, _, rule_files in os.walk(DIR_RULES):
         for rule_file in rule_files:
             rule_name = rule_file.split('.')[0]
@@ -193,20 +205,48 @@ def test_kinesis_alert_rules():
             if not test_records:
                 LOGGER_CLI.error('Improperly formatted test file: %s', rule_file_path)
                 continue
-
-            if len(test_records) == 0:
+            elif len(test_records) == 0:
                 LOGGER_CLI.error('No records to test for %s', rule_name)
                 continue
 
-            for test_record in test_records:
+            # Go backwards over the records so we can remove improper ones
+            # safely without unnecessary copying/modifying of the list
+            for test_record in reversed(test_records):
                 if not check_keys(test_record):
-                    LOGGER_CLI.error('Improperly formatted test_record: %s',
-                                    test_record)
+                    LOGGER_CLI.error('Discarding improperly formatted record for service %s: %s',
+                                     test_record['service'],
+                                     test_record)
+                    # Removing an improperly formatted record here allows us to
+                    # continue with current tests, while still logging it above
+                    test_records.pop(test_records.index(test_record))
                     continue
+
                 apply_helpers(test_record)
-                test_record['kinesis_data'] = format_record(test_record)
+                service_record_key = '{}_record'.format(test_record['service'])
+                test_record[service_record_key] = format_record(test_record)
 
             test_rule(rule_name, contents)
+
+    BOTO_MOCKER.stop()
+
+def put_mocked_s3_object(bucket_name, key_name, body_value):
+    """Create a mock AWS S3 object for testing
+
+    Args:
+        bucket_name: the bucket in which to place the object (string)
+        key_name: the key to use for the S3 object (string)
+        body_value: the actual value to use for the object (string)
+    """
+    s3_resource = boto3.resource('s3', region_name='us-east-1')
+    s3_resource.create_bucket(Bucket=bucket_name)
+    obj = s3_resource.Object(bucket_name, key_name)
+    response = obj.put(Body=body_value)
+
+    # Log if this was not a success (this should not fail for mocked objects)
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        LOGGER_CLI.error('Could not put mock object with key %s in s3 bucket with name %s',
+                         key_name,
+                         bucket_name)
 
 def stream_alert_test(options):
     """Integration testing handler
@@ -219,15 +259,9 @@ def stream_alert_test(options):
     else:
         LOGGER_SA.setLevel(logging.INFO)
 
-    if options.source == 'kinesis':
-        if options.func == 'alert':
-            test_kinesis_alert_rules()
+    if options.func == 'alert':
+        test_alert_rules()
 
-        elif options.func == 'output':
-            # TODO(jack) handle s3 event formatting
-            pass
-
-    elif options.source == 's3':
-        if options.func == 'alert':
-            # TODO(jack) handle s3 event formatting
-            pass
+    elif options.func == 'output':
+        # TODO(jack) test output
+        raise NotImplementedError
