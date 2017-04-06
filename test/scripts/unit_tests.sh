a50e0bfe013068efa5f52d0cfa701f83218709dd
Jack Naglieri <jack.naglieri@airbnb.com>
2017-04-05 17:02:21 -0700

    [test][ci] enforce 80% code coverage and add coverage report for stream_alert_cli

--- test/scripts/unit_tests.sh
+++ test/scripts/unit_tests.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-nosetests test/unit --with-coverage --cover-package=stream_alert
+nosetests test/unit --with-coverage --cover-package=stream_alert --cover-package=stream_alert_cli --cover-min-percentage=80
