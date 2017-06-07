740a8bd577aaec6224a98847066c8b723359c63d
Jack Naglieri <jack.naglieri@airbnb.com>
2017-06-06 18:06:10 -0700

    [tf modules][cloudtrail] enable s3 versioning, use iam_policy_document data resources

--- terraform/modules/tf_stream_alert_cloudtrail/iam.tf
+++ terraform/modules/tf_stream_alert_cloudtrail/iam.tf
@@ -1,45 +1,41 @@
-// Role for CloudWatch events
+// IAM Role: CloudWatch Events
 resource "aws_iam_role" "streamalert_cloudwatch_role" {
   name = "${var.prefix}_${var.cluster}_streamalert_cloudwatch_role"
   path = "/streamalert/"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "events.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = "${data.aws_iam_policy_document.streamalert_cloudwatch_role_assume_role_policy.json}"
 }
 
-// Policy for CloudWatch to allow write access to Kinesis
+data "aws_iam_policy_document" "streamalert_cloudwatch_role_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+// IAM Policy: Allow CloudWatch to write events to Kinesis Streams
 resource "aws_iam_role_policy" "streamalert_cloudwatch_policy" {
   name = "${var.prefix}_${var.cluster}_streamalert_cloudwatch"
   role = "${aws_iam_role.streamalert_cloudwatch_role.id}"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "kinesis:PutRecord",
-                "kinesis:PutRecords"
-            ],
-            "Resource": [
-                "${var.kinesis_arn}"
-            ]
-        }
-    ]
+  policy = "${data.aws_iam_policy_document.kinesis_put_records.json}"
 }
-EOF
+
+data "aws_iam_policy_document" "kinesis_put_records" {
+  statement {
+    sid = "CloudWatchEventsPutKinesisRecords"
+
+    actions = [
+      "kinesis:PutRecord",
+      "kinesis:PutRecords",
+    ]
+
+    resources = [
+      "${var.kinesis_arn}",
+    ]
+  }
 }
