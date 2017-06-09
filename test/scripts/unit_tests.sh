7f08285fc5d69685432d58aa7d5f9ca63204ab5b
Ryan Deivert <ryan.deivert@airbnb.com>
2017-06-08 22:12:54 -0700

    [tests] adding the `alert_processor` package to nose tests & coverage

--- test/scripts/unit_tests.sh
+++ test/scripts/unit_tests.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-nosetests test/unit --with-coverage --cover-package=stream_alert.rule_processor --cover-package=stream_alert_cli --cover-min-percentage=80
+nosetests test/unit --with-coverage --cover-package=stream_alert.rule_processor --cover-package=stream_alert.alert_processor --cover-package=stream_alert_cli --cover-min-percentage=80
