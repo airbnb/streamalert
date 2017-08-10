58368a35226962d0aad07befd608adb01b4ad1fa
Austin Byers <austin.byers@airbnb.com>
2017-08-10 11:04:00 -0700

    pylint metrics library

--- tests/unit/stream_alert_shared/test_metrics.py
+++ tests/unit/stream_alert_shared/test_metrics.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright 2017-present, Airbnb Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,13 +12,12 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-'''
+"""
+# pylint: disable=protected-access
+from botocore.exceptions import ClientError
 from mock import Mock, patch
 
-from botocore.exceptions import ClientError
-
 from stream_alert.shared.metrics import Metrics
-
 from tests.unit.stream_alert_rule_processor import REGION
 
 
