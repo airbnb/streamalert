97591b36b3ed2ba915c48cd4691a98c65c64cfef
x <x@airbnb.com>
2017-10-02 10:56:48 -0700

    osquery - batch

--- rules/community/duo_authentication/duo_anonymous_ip_failure.py
+++ rules/community/duo_authentication/duo_anonymous_ip_failure.py
@@ -1,8 +1,7 @@
-"""Alert on any Duo authentication logs marked as a failure due to an Anonymous IP."""
+"""Alert on any Duo auth logs marked as a failure due to an Anonymous IP."""
 from stream_alert.rule_processor.rules_engine import StreamRules
 
 rule = StreamRules.rule
-
 
 @rule(logs=['duo:authentication'],
       outputs=['aws-s3:sample-bucket',
@@ -11,8 +10,8 @@ rule = StreamRules.rule
 def duo_anonymous_ip_failure(rec):
     """
     author:       airbnb_csirt
-    description:  Alert on any Duo authentication logs marked as a failure due to an Anonymous IP.
-    reference:    N/A
+    description:  Alert on Duo auth logs marked as a failure due to an Anonymous IP.
+    reference:    https://duo.com/docs/policy#anonymous-networks
     playbook:     N/A
     """
     return rec['result'] == 'FAILURE' and rec['reason'] == 'Anonymous IP'
