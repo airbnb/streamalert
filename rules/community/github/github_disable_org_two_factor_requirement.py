12ab7c8f9b4b7bbf1b61102ab200ab5e924b175b
x <x@airbnb.com>
2017-10-02 16:48:50 -0700

    fix 100 char travis warning

--- rules/community/github/github_disable_org_two_factor_requirement.py
+++ rules/community/github/github_disable_org_two_factor_requirement.py
@@ -13,7 +13,8 @@ def github_disable_org_two_factor_requirement(rec):
     """
     author:       @mimeframe
     description:  Two-factor authentication requirement was disabled.
-    reference:    https://help.github.com/articles/requiring-two-factor-authentication-in-your-organization/
+    reference:    https://help.github.com/
+                  articles/requiring-two-factor-authentication-in-your-organization/
     """
     message_rec = ghe_json_message(rec)
     if not message_rec:
