ea2784ba107d1b666cd9f2b9411619f47580c612
javier_marcos <javier.marcos@airbnb.com>
2017-11-08 11:19:24 -0800

    Adding pagerdutyv2 to choices in manage.py

--- manage.py
+++ manage.py
@@ -102,7 +102,7 @@ Examples:
     # Output service options
     output_parser.add_argument(
         '--service',
-        choices=['aws-lambda', 'aws-s3', 'pagerduty', 'phantom', 'slack'],
+        choices=['aws-lambda', 'aws-s3', 'pagerduty', 'pagerdutyv2', 'phantom', 'slack'],
         required=True,
         help=ARGPARSE_SUPPRESS
     )
