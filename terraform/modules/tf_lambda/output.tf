3dd602f0ea3101274c22b0d04eab61dc73b496bd
Austin Byers <mrmathbyers@gmail.com>
2018-02-14 15:00:52 -0800

    Create re-usable Lambda Terraform module (#596)

---
+++ terraform/modules/tf_lambda/output.tf
@@ -0,0 +1,17 @@
+// Defined only if the Lambda is in a VPC
+output "function_vpc_arn" {
+  value = "${aws_lambda_function.function_vpc.arn}"
+}
+
+// Defined only if the Lambda is NOT in a VPC
+output "function_no_vpc_arn" {
+  value = "${aws_lambda_function.function_no_vpc.arn}"
+}
+
+output "role_arn" {
+  value = "${aws_iam_role.role.arn}"
+}
+
+output "role_id" {
+  value = "${aws_iam_role.role.id}"
+}
