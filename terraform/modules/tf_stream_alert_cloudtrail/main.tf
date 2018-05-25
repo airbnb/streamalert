2e2a3a11326f52c78fe3772322a3770a8f311c61
ryandeivert <ryandeivert@gmail.com>
2018-05-25 09:42:12 -0700

    [tf] fixing bug with policy resource value (#752)
    * [tf] fixing bug with policy resource value
    
    * [tf] using destination arn instead of kinesis arn in subscription filter

--- terraform/modules/tf_stream_alert_cloudtrail/main.tf
+++ terraform/modules/tf_stream_alert_cloudtrail/main.tf
@@ -124,8 +124,7 @@ resource "aws_cloudwatch_log_subscription_filter" "cloudtrail_via_cloudwatch" {
   name            = "cloudtrail_delivery"
   log_group_name  = "${aws_cloudwatch_log_group.cloudtrail_logging.name}"
   filter_pattern  = "${var.exclude_home_region_events ? local.apply_filter_string : ""}"
-  role_arn        = "${var.subscription_role_arn}"
-  destination_arn = "${var.kinesis_arn}"
+  destination_arn = "${var.cw_destination_arn}"
   distribution    = "Random"
 }
 
