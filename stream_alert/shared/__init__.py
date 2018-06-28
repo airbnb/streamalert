680507175e4a9a2f945465858997e8f5cb41fd0a
Ryan Deivert <ryan.deivert@airbnb.com>
2018-06-27 22:11:57 -0700

    rule promotion init

--- stream_alert/shared/__init__.py
+++ stream_alert/shared/__init__.py
@@ -7,6 +7,7 @@ ALERT_MERGER_NAME = 'alert_merger'
 ALERT_PROCESSOR_NAME = 'alert_processor'
 ATHENA_PARTITION_REFRESH_NAME = 'athena_partition_refresh'
 RULE_PROCESSOR_NAME = 'rule_processor'
+RULE_PROMOTION_NAME = 'rule_promotion'
 NORMALIZATION_KEY = 'streamalert:normalization'
 
 # Create a package level logger to import
