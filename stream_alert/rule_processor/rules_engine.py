1735fada1a497440f166a9f78164e2dedc77ffee
Jeremy Stott <jeremy@stott.co.nz>
2018-07-19 03:14:38 +0800

    [core] allow req_subkeys with none and false values (#788)
    * [core] allow req_subkeys with none and false values
    
    * Required subkeys with none or false values should still cause the rule to be run.
    * Using any() instead of all() should be faster, as any() can exit early if a subkey is missing.
    * Added test_process_subkeys_false_none_value to test None and False values for subkeys.
    
    * [tests] fixed pylint (singleton-comparison)
    
    * [core] check required subkey object is a dictionary
    
    * Make sure the required subkey object is a dictionary before attempting to check if the subkeys exist.
    * Added unit test test_process_subkeys_non_dict to cover non-dictionary type subkey objects.

--- stream_alert/rule_processor/rules_engine.py
+++ stream_alert/rule_processor/rules_engine.py
@@ -235,7 +235,12 @@ class RulesEngine(object):
                     'The required subkey %s is not found when trying to process %s: \n%s',
                     key, rule.name, record)
                 return False
-            if not all(record[key].get(x) for x in nested_keys):
+            if not isinstance(record.get(key), dict):
+                LOGGER.debug(
+                    'The required subkey %s is not a dictionary when trying to process %s: \n%s',
+                    key, rule.name, record)
+                return False
+            if any(x not in record[key] for x in nested_keys):
                 return False
 
         return True
