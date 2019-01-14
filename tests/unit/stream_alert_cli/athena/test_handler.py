3aea1625caa0fd245c43d9fd0e5a71e47de8ecd5
Garret Reece <GarretReece@users.noreply.github.com>
2019-01-14 14:06:20 -0600

    fixing up unit tests

--- tests/unit/stream_alert_cli/athena/test_handler.py
+++ tests/unit/stream_alert_cli/athena/test_handler.py
@@ -31,8 +31,8 @@ def test_construct_create_table_statement():
     }
 
     expected_result = ('CREATE EXTERNAL TABLE table-name (value01 string, value02 integer, '
-                       'value03 struct<value04:boolean, value05:float>) PARTITIONED BY '
-                       '(dt string) STORED AS PARQUET '
+                       'value03 struct<value04:boolean, value05:float>) '
+                       'STORED AS PARQUET '
                        'LOCATION \'s3://bucket-name/table-name/\'')
 
     result = handler._construct_create_table_statement(schema, 'table-name', 'bucket-name')
