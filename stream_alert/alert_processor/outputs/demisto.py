5788bcad9dc87bc4f34cea59b0c037ba8d0d075a
Derek Wang <derek.wang@airbnb.com>
2019-02-08 16:35:25 -0800

    pylint

--- stream_alert/alert_processor/outputs/demisto.py
+++ stream_alert/alert_processor/outputs/demisto.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from collections import OrderedDict
-import json
 
 from stream_alert.alert_processor.outputs.output_base import (
     OutputDispatcher,
