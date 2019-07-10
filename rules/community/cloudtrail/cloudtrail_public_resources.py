81f83ffb2f3aa284f5bcf951b10164fd3efae15b
Scott Piper <scott@summitroute.com>
2019-07-10 16:09:52 -0600

    Detect resources made public

---
+++ rules/community/cloudtrail/cloudtrail_public_resources.py
@@ -0,0 +1,75 @@
+"""Alert when resources are made public."""
+from stream_alert.shared.rule import rule
+from policyuniverse.policy import Policy
+import json
+
+
+@rule(logs=['cloudtrail:events'])
+def cloudtrail_public_resources(rec):
+    """
+    author:           spiper
+    description:      Detect resources being made public.
+    
+    playbook:         (a) identify the AWS account in the log
+                      (b) identify what resource(s) are impacted by the API call
+                      (c) determine if the intent is valid, malicious or accidental
+    """
+    # Get the policy string for each resource
+    policy_string = ''
+
+    # Check S3
+    if rec['eventName'] == 'PutBucketPolicy':
+        # S3 doesn't use a policy string, but actual json, unlike all
+        # other commands
+        policy = rec.get('requestParameters', {}).get('bucketPolicy', None)
+        if policy is None:
+            return False
+        policy = Policy(policy)
+        if policy.is_internet_accessible():
+            return True
+
+    # Check ElasticSearch
+    if rec['eventName'] == 'CreateElasticsearchDomain':
+        policy_string = rec.get('requestParameters', {}).get('accessPolicies', '')
+    if rec['eventName'] == 'UpdateElasticsearchDomainConfig':
+        policy_string = rec.get('requestParameters', {}).get('accessPolicies', '')
+    
+    # Check Glacier Vaults
+    if rec['eventName'] == 'SetVaultAccessPolicy':
+        policy_string = rec.get('requestParameters', {}).get('policy', {}).get('policy', '')
+    
+    # Check SQS
+    if rec['eventName'] == 'SetQueueAttributes':
+        policy_string = rec.get('requestParameters', {}).get('attributes', {}).get('Policy', '')
+
+    # Check SNS
+    if rec['eventName'] == 'SetTopicAttributes':
+        if rec.get('requestParameters', {}).get('attributeName', '') == 'Policy':
+            policy_string = rec['requestParameters'].get('attributeValue','')
+    if rec['eventName'] == 'CreateTopic':
+        policy_string = rec.get('requestParameters', {}).get('attributes', '').get('Policy', '')
+    
+    # Check ECR
+    if rec['eventName'] == 'SetRepositoryPolicy':
+        policy_string = rec.get('requestParameters', {}).get('policyText', '')
+    
+    # Check KMS
+    if rec['eventName'] == 'PutKeyPolicy':
+        policy_string = rec.get('requestParameters', {}).get('policy', '')
+    if rec['eventName'] == 'CreateKey':
+        policy_string = rec.get('requestParameters', {}).get('policy', '')
+    
+    # Check SecretsManager
+    if rec['eventName'] == 'PutResourcePolicy':
+        policy_string = rec.get('requestParameters', {}).get('resourcePolicy', '')
+
+    # Check the policy
+    if policy_string != '':    
+        policy = json.loads(policy_string)
+        policy = Policy(policy)
+        if policy.is_internet_accessible():
+            return True
+
+    return False
+    
+
---
+++ rules/community/cloudtrail/cloudtrail_snapshot_or_ami_made_public.py
@@ -0,0 +1,45 @@
+"""Alert on destructive AWS API calls."""
+from stream_alert.shared.rule import rule
+
+@rule(logs=['cloudtrail:events'])
+def cloudtrail_snapshot_or_ami_made_public(rec):
+    """
+    author:           spiper
+    description:      Alert on AWS API calls that make EBS snapshots, 
+                        RDS snapshots, or AMIs public.
+    playbook:         (a) identify the AWS account in the log
+                      (b) identify what resource(s) are impacted by the API call
+                      (c) determine if the intent is valid, malicious or accidental
+    """
+
+    # For each resouce walk through through the request parameters to check
+    # if this is adding permissions for "all"
+
+    # Check AMIs
+    if rec['eventName'] == 'ModifyImageAttribute':
+        # For AMIs
+        params = rec.get('requestParameters', {}) 
+        if params.get('attributeType', '') == 'launchPermission':
+            if 'add' in params.get('launchPermission', {}):
+                items = params['launchPermission']['add']['items']
+                for item in items:
+                    if item.get('group', '') == 'all':
+                        return True
+
+    # Check EBS snapshots
+    if rec['eventName'] == 'ModifySnapshotAttribute':
+        params = rec.get('requestParameters', {}) 
+        if params.get('attributeType', '') == 'CREATE_VOLUME_PERMISSION':
+            if 'add' in params.get('createVolumePermission', {}):
+                items = params['createVolumePermission']['add']['items']
+                for item in items:
+                    if item.get('group', '') == 'all':
+                        return True
+    
+    # Check RDS snapshots
+    if rec['eventName'] == 'ModifyDBClusterSnapshotAttribute':
+        params = rec.get('requestParameters', {}) 
+        if 'all' in params.get('valuesToAdd', []):
+            return True
+    
+    return False
