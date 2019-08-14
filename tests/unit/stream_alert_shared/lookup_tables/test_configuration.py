d8ccedb8b216d9610480b0f6b3c40f998fe312ef
Derek Wang <derek.wang@airbnb.com>
2019-08-13 18:07:12 -0700

    First in a long line of tests

---
+++ tests/unit/stream_alert_shared/lookup_tables/test_configuration.py
@@ -0,0 +1,163 @@
+"""
+Copyright 2017-present, Airbnb Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import copy
+from datetime import datetime, timedelta
+import json
+
+from nose.tools import (
+    assert_equal,
+    assert_false,
+    assert_is_instance,
+    assert_not_in,
+    assert_raises,
+    assert_true
+)
+
+from stream_alert.shared.lookup_tables.configuration import LookupTablesConfiguration
+
+
+class TestLookupTablesConfiguration(object):
+    """Test shared LookupTablesConfiguration class."""
+
+    # pylint: disable=no-self-use,protected-access,too-many-public-methods
+
+    @staticmethod
+    def _basic_configuration():
+        return {
+            "lookup_tables": {
+                "enabled": True,
+                "tables": {
+                    "resource_map_prototype": {
+                        "driver": "s3",
+                        "bucket": "airbnb.sample.lookuptable",
+                        "key": "resource_map.gz",
+                        "cache_refresh_minutes": 10,
+                        "compression": "gzip"
+                    },
+                    "resource_map_dynamodb": {
+                        "driver": "dynamodb",
+                        "table": "some_table_name",
+                        "partition_key": "MyPartitionKey",
+                        "value_key": "MyValueKey",
+                        "cache": {
+                            "refresh_minutes": 2,
+                            "maximum_keys": 10
+                        }
+                    }
+                }
+            },
+            "global": {
+                "infrastructure": {
+                    "lookup_tables": {
+                        "buckets": {
+                            "airbnb.sample.lookuptable": [
+                                "resource_map_legacy.gz"
+                            ]
+                        },
+                        "cache_refresh_minutes": 10,
+                        "enabled": True
+                    }
+                }
+            }
+        }
+
+    def test_configuration_enabled(self):
+        """LookupTablesConfiguration - Basic Configuration - Enabled"""
+        config = LookupTablesConfiguration(self._basic_configuration())
+        assert_true(config.is_enabled)
+
+    def test_configuration_table_names(self):
+        """LookupTablesConfiguration - Basic Configuration - Table Names"""
+        config = LookupTablesConfiguration(self._basic_configuration())
+        assert_equal(
+            config.table_names,
+            ['resource_map_prototype', 'resource_map_dynamodb', 'resource_map_legacy']
+        )
+
+    def test_configuration_table_configurations(self):
+        """LookupTablesConfiguration - Basic Configuration - Table Configurations"""
+        config = LookupTablesConfiguration(self._basic_configuration())
+        assert_equal(config.table_configurations['resource_map_dynamodb']['driver'], 'dynamodb')
+        assert_equal(config.table_configurations['resource_map_legacy']['driver'], 's3')
+
+    def test_configuration_empty_configuration_not_enabled(self):
+        """LookupTablesConfiguration - Empty Configuration - Not Enabled"""
+        config = LookupTablesConfiguration({})
+        assert_false(config.is_enabled)
+
+    def test_configuration_conflict_configuration(self):
+        """LookupTablesConfiguration - Conflict Configuration - Raises Exception"""
+        config = {
+            "lookup_tables": {
+                "enabled": True,
+                "tables": {
+                    "resource_map_prototype": {
+                        "driver": "s3",
+                        "bucket": "airbnb.sample.lookuptable",
+                        "key": "resource_map.gz",
+                        "cache_refresh_minutes": 10,
+                        "compression": "gzip"
+                    },
+                }
+            },
+            "global": {
+                "infrastructure": {
+                    "lookup_tables": {
+                        "buckets": {
+                            "airbnb.sample.lookuptable": [
+                                "resource_map_legacy.gz",
+                                "resource_map_prototype.gz",  # Duplicate
+                            ]
+                        },
+                        "cache_refresh_minutes": 10,
+                        "enabled": True
+                    }
+                }
+            }
+        }
+        assert_raises(Exception, LookupTablesConfiguration, config)
+
+    def test_configuration_disabled_configuration(self):
+        """LookupTablesConfiguration - Disabled Configuration - Disabled"""
+        config = LookupTablesConfiguration({
+            "lookup_tables": {
+                "enabled": False,
+                "tables": {
+                    "resource_map_prototype": {
+                        "driver": "s3",
+                        "bucket": "airbnb.sample.lookuptable",
+                        "key": "resource_map.gz",
+                        "cache_refresh_minutes": 10,
+                        "compression": "gzip"
+                    },
+                }
+            },
+            "global": {
+                "infrastructure": {
+                    "lookup_tables": {
+                        "buckets": {
+                            "airbnb.sample.lookuptable": [
+                                "resource_map_legacy.gz",
+                                "resource_map_prototype.gz",  # Duplicate
+                            ]
+                        },
+                        "cache_refresh_minutes": 10,
+                        "enabled": False
+                    }
+                }
+            }
+        })
+        assert_false(config.is_enabled)
