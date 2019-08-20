15cf6b043855cededbda25672410fb6a7ab4fb33
Derek Wang <derek.wang@airbnb.com>
2019-08-19 17:30:50 -0700

    PR feedback fixup

--- tests/unit/stream_alert_shared/lookup_tables/test_driver_s3.py
+++ tests/unit/stream_alert_shared/lookup_tables/test_driver_s3.py
@@ -23,7 +23,7 @@ from moto import mock_s3
 from nose.tools import assert_equal, assert_raises
 
 from stream_alert.shared.config import load_config
-from stream_alert.shared.lookup_tables.drivers import construct_persistence_driver
+from stream_alert.shared.lookup_tables.drivers_factory import construct_persistence_driver
 from stream_alert.shared.lookup_tables.errors import LookupTablesInitializationError
 from tests.unit.helpers.aws_mocks import put_mock_s3_object
 
--- tests/unit/stream_alert_shared/lookup_tables/test_table.py
+++ tests/unit/stream_alert_shared/lookup_tables/test_table.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 from nose.tools import assert_equal
 
-from stream_alert.shared.lookup_tables.drivers import construct_persistence_driver
+from stream_alert.shared.lookup_tables.drivers_factory import construct_persistence_driver
 from stream_alert.shared.lookup_tables.table import LookupTable
 
 
