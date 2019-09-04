99d40fc14bd5833f4abf7a398b8f10d8fa8afd5b
Ryxias <derek.wang@airbnb.com>
2019-09-03 17:12:32 -0700

    [LookupTables] Phase 1 implementation of LookupTables rebuild (#969)
    * First pass for lookuptables
    
    * First in a long line of tests
    
    * Metaprogramming hell
    
    * Removes old code
    
    * Working through kinks and whatever
    
    * Better messaging, better reliability
    
    * Tests and such
    
    * Tests for the S3 driver
    
    * It's coming together
    
    * Making more progress on dynamodb driver
    
    * Getting closer
    
    * Deletes old LookupTables
    
    * Maybe maybe maybe
    
    * Theres gotta bea better way
    
    * Gottem tests
    
    * Add tests for proving the concept of multi-lookuptables on a single DynamoDB Table
    
    * Add lots of tests and such
    
    * Ready for testing
    
    * A comment
    
    * Amending pylint errors
    
    * DRYS out DynamoDb mock testing code
    
    * Missing region
    
    * ?
    
    * adds cache classes prior to integrating them
    
    * Fixup
    
    * Ports over dynamodb driver
    
    * Ports S3 driver to cache
    
    * Pylint
    
    * Cache eviction
    
    * PR feedback fixup
    
    * pr feedback
    
    * Updates to fix python3
    
    * Pylint

---
+++ stream_alert/shared/lookup_tables/errors.py
@@ -0,0 +1,15 @@
+
+class LookupTablesError(RuntimeError):
+    """Generic class for errors raised from LookupTables systems"""
+
+
+class LookupTablesInitializationError(LookupTablesError):
+    """Any error raised when a specific table/driver is attempting to initialize"""
+
+
+class LookupTablesCommitError(LookupTablesError):
+    """Any error raised when a LookupTable or driver fails to successfully commit changes"""
+
+
+class LookupTablesConfigurationError(LookupTablesError):
+    """Errors raised that detect a misconfiguration for any LookupTables system"""
