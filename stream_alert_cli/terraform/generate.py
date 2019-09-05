40ef2db6d81477da81aa5019b924559d0412c43e
Derek Wang <derek.wang@airbnb.com>
2019-09-05 10:58:36 -0700

    DRYs out terraform module generation code

--- stream_alert_cli/terraform/generate.py
+++ stream_alert_cli/terraform/generate.py
@@ -389,13 +389,7 @@ def terraform_generate_handler(config, init=False, check_tf=True, check_creds=Tr
 
     # Setup the main.tf.json file
     LOGGER.debug('Generating cluster file: main.tf.json')
-    with open('terraform/main.tf.json', 'w') as tf_file:
-        json.dump(
-            generate_main(config, init=init),
-            tf_file,
-            indent=2,
-            sort_keys=True
-        )
+    _create_terraform_module_file(generate_main(config, init=init), 'terraform/main.tf.json')
 
     # Return early during the init process, clusters are not needed yet
     if init:
@@ -414,23 +408,15 @@ def terraform_generate_handler(config, init=False, check_tf=True, check_creds=Tr
                 'An error was generated while creating the %s cluster', cluster)
             return False
 
-        with open('terraform/{}.tf.json'.format(cluster), 'w') as tf_file:
-            json.dump(
-                cluster_dict,
-                tf_file,
-                indent=2,
-                sort_keys=True
-            )
+        _create_terraform_module_file(cluster_dict, 'terraform/{}.tf.json'.format(cluster))
 
     metric_filters = generate_aggregate_cloudwatch_metric_filters(config)
     if metric_filters:
-        with open('terraform/metric_filters.tf.json', 'w') as tf_file:
-            json.dump(metric_filters, tf_file, indent=2, sort_keys=True)
+        _create_terraform_module_file(metric_filters, 'terraform/metric_filters.tf.json')
 
     metric_alarms = generate_aggregate_cloudwatch_metric_alarms(config)
     if metric_alarms:
-        with open('terraform/metric_alarms.tf.json', 'w') as tf_file:
-            json.dump(metric_alarms, tf_file, indent=2, sort_keys=True)
+        _create_terraform_module_file(metric_alarms, 'terraform/metric_alarms.tf.json')
 
     # Setup Athena
     generate_global_lambda_settings(
@@ -536,8 +522,7 @@ def _generate_lookup_tables_settings(config):
             }
         }
     }
-    with open(tf_file_name, 'w') as tf_file:
-        json.dump(generated_config, tf_file, indent=2, sort_keys=True)
+    _create_terraform_module_file(generated_config, tf_file_name)
 
 
 def generate_global_lambda_settings(config, config_name, generate_func, tf_tmp_file, message):
@@ -558,8 +543,7 @@ def generate_global_lambda_settings(config, config_name, generate_func, tf_tmp_f
     if config['lambda'][config_name].get('enabled', True):
         generated_config = generate_func(config=config)
         if generated_config:
-            with open(tf_tmp_file, 'w') as tf_file:
-                json.dump(generated_config, tf_file, indent=2, sort_keys=True)
+            _create_terraform_module_file(generated_config, tf_tmp_file)
     else:
         remove_temp_terraform_file(tf_tmp_file, message)
 
@@ -614,3 +598,12 @@ def _generate_global_module(config):
 
 def _config_get_logging_bucket(config):
     return config['global']['s3_access_logging']['logging_bucket']
+
+
+def _create_terraform_module_file(generated_config, filename):
+    """
+    Dumps the given generated_config, a JSON dict, into the given filename under the terraform/
+    directory, as a .tf.json file.
+    """
+    with open(filename, 'w') as file:
+        json.dump(generated_config, file, indent=2, sort_keys=True)
