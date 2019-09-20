fb25625d2c1b269d6dde881769db30c6084ddcce
Ryan Deivert <ryan.deivert@airbnb.com>
2019-09-20 10:17:09 -0700

    adding default value for metric to tf_metric_filters module

--- terraform/modules/tf_metric_filters/main.tf
+++ terraform/modules/tf_metric_filters/main.tf
@@ -5,8 +5,9 @@ resource "aws_cloudwatch_log_metric_filter" "cloudwatch_metric_filters" {
   log_group_name = "${var.log_group_name}"
 
   metric_transformation {
-    name  = "${var.metric_name}"
-    value = "${var.metric_value}"
+    name          = "${var.metric_name}"
+    value         = "${var.metric_value}"
+    default_value = "${var.metric_default_value}"
 
     namespace = "StreamAlert"
   }
