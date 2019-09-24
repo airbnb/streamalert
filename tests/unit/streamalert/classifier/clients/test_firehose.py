airbnb/streamalert

commit dc44ba5a67edf02f7974d4c0b4914ba1437b6ba5

adding prefix to firehoses and other small updates (#1022)
* removing athena default db name to enforce that one is provided

* formatting nit

* adding default value for metric to tf_metric_filters module

* prefixing firehoses created for data

* adding missing test docstring

* making prefix for unit testing consistent

* adding prefixing support for data firehoses

streamalert/classifier/clients/firehose.py
@@ -51,8 +51,8 @@ class FirehoseClient:
     # The subtraction of 2 accounts for the newline at the end
     MAX_RECORD_SIZE = 1000 * 1000 - 2
 
-    # Default firehose name prefix
-    DEFAULT_FIREHOSE_PREFIX = 'streamalert_data_{}'
+    # Default firehose name format, should be formatted with deployment prefix
+    DEFAULT_FIREHOSE_FMT = '{}_streamalert_data_{}'
 
     # Exception for which backoff operations should be performed
     EXCEPTIONS_TO_BACKOFF = (ClientError, BotocoreConnectionError, HTTPClientError)
@@ -60,7 +60,8 @@ class FirehoseClient:
     # Set of enabled log types for firehose, loaded from configs
     _ENABLED_LOGS = dict()
 
-    def __init__(self, firehose_config=None, log_sources=None):
+    def __init__(self, prefix, firehose_config=None, log_sources=None):
+        self._prefix = prefix
         self._client = boto3.client('firehose', config=boto_helpers.default_config())
         self.load_enabled_log_sources(firehose_config, log_sources, force_load=True)
 
@@ -357,7 +358,7 @@ class FirehoseClient:
         return cls._ENABLED_LOGS
 
     @classmethod
-    def load_from_config(cls, firehose_config, log_sources):
+    def load_from_config(cls, prefix, firehose_config, log_sources):
         """Get a Firehose client for sending logs
 
         Args:
@@ -370,7 +371,7 @@ class FirehoseClient:
         """
         if not firehose_config.get('enabled'):
             return
-        return cls(firehose_config=firehose_config, log_sources=log_sources)
+        return cls(prefix=prefix, firehose_config=firehose_config, log_sources=log_sources)
 
     def send(self, payloads):
         """Send all classified records to a respective Firehose Delivery Stream
@@ -386,7 +387,7 @@ class FirehoseClient:
         for log_type, records in records.items():
             # This same substitution method is used when naming the Delivery Streams
             formatted_log_type = self.firehose_log_name(log_type)
-            stream_name = self.DEFAULT_FIREHOSE_PREFIX.format(formatted_log_type)
+            stream_name = self.DEFAULT_FIREHOSE_FMT.format(self._prefix, formatted_log_type)
 
             # Process each record batch in the categorized payload set
             for record_batch in self._record_batches(records):

terraform/modules/tf_kinesis_firehose_delivery_stream/main.tf
@@ -5,7 +5,7 @@
 //
 // This is a less destructive approach to creating all of the Streams.
 resource "aws_kinesis_firehose_delivery_stream" "streamalert_data" {
-  name        = "streamalert_data_${var.log_name}"
+  name        = "${var.prefix}_streamalert_data_${var.log_name}"
   destination = "s3"
 
   s3_configuration {
@@ -26,7 +26,7 @@ resource "aws_kinesis_firehose_delivery_stream" "streamalert_data" {
 // AWS CloudWatch Metric Alarm for this Firehose
 resource "aws_cloudwatch_metric_alarm" "firehose_records_alarm" {
   count               = "${var.enable_alarm ? 1 : 0}"
-  alarm_name          = "streamalert_data_${var.log_name}_record_count"
+  alarm_name          = "${aws_kinesis_firehose_delivery_stream.streamalert_data.name}_record_count"
   namespace           = "AWS/Firehose"
   metric_name         = "IncomingRecords"
   statistic           = "Sum"

tests/unit/streamalert/classifier/clients/test_firehose.py
@@ -27,7 +27,7 @@ class TestFirehoseClient:
     def setup(self):
         """Setup before each method"""
         with patch('boto3.client'):  # patch to speed up unit tests slightly
-            self._client = FirehoseClient()
+            self._client = FirehoseClient(prefix='unit-test')
 
     def teardown(self):
         """Teardown after each method"""
@@ -381,12 +381,21 @@ class TestFirehoseClient:
     def test_load_from_config(self):
         """FirehoseClient - Load From Config"""
         with patch('boto3.client'):  # patch to speed up unit tests slightly
-            client = FirehoseClient.load_from_config({'enabled': True}, None)
+            client = FirehoseClient.load_from_config(
+                prefix='unit-test',
+                firehose_config={'enabled': True},
+                log_sources=None
+            )
             assert_equal(isinstance(client, FirehoseClient), True)
 
     def test_load_from_config_disabled(self):
         """FirehoseClient - Load From Config, Disabled"""
-        assert_equal(FirehoseClient.load_from_config({}, None), None)
+        client = FirehoseClient.load_from_config(
+            prefix='unit-test',
+            firehose_config={},
+            log_sources=None
+        )
+        assert_equal(client, None)
 
     @patch.object(FirehoseClient, '_send_batch')
     def test_send(self, send_batch_mock):
@@ -400,5 +409,5 @@ class TestFirehoseClient:
         ]
         self._client.send(self._sample_payloads)
         send_batch_mock.assert_called_with(
-            'streamalert_data_log_type_01_sub_type_01', expected_batch
+            'unit-test_streamalert_data_log_type_01_sub_type_01', expected_batch
         )

tests/unit/streamalert_cli/terraform/test_firehose.py
@@ -31,10 +31,10 @@ class TestFirehoseGenerate:
         return {
             'source': 'modules/tf_kinesis_firehose_setup',
             'account_id': "12345678910",
-            'prefix': 'unit-testing',
+            'prefix': 'unit-test',
             'region': 'us-west-1',
             's3_logging_bucket': self._logging_bucket_name,
-            's3_bucket_name': 'unit-testing.streamalert.data',
+            's3_bucket_name': 'unit-test.streamalert.data',
             'kms_key_id': '${aws_kms_key.server_side_encryption.key_id}'
         }
 
@@ -72,7 +72,7 @@ class TestFirehoseGenerate:
                     'compression_format': 'GZIP',
                     'log_name': 'json_embedded',
                     'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
-                    's3_bucket_name': 'unit-testing.streamalert.data',
+                    's3_bucket_name': 'unit-test.streamalert.data',
                     'kms_key_arn': '${aws_kms_key.server_side_encryption.arn}'
                 }
             }
@@ -103,11 +103,11 @@ class TestFirehoseGenerate:
                     'compression_format': 'GZIP',
                     'log_name': 'json_embedded',
                     'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
-                    's3_bucket_name': 'unit-testing.streamalert.data',
+                    's3_bucket_name': 'unit-test.streamalert.data',
                     'kms_key_arn': '${aws_kms_key.server_side_encryption.arn}',
                     'enable_alarm': True,
                     'alarm_actions': [
-                        'arn:aws:sns:us-west-1:12345678910:unit-testing_streamalert_monitoring'
+                        'arn:aws:sns:us-west-1:12345678910:unit-test_streamalert_monitoring'
                     ]
                 }
             }
@@ -141,14 +141,14 @@ class TestFirehoseGenerate:
                     'compression_format': 'GZIP',
                     'log_name': 'json_embedded',
                     'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
-                    's3_bucket_name': 'unit-testing.streamalert.data',
+                    's3_bucket_name': 'unit-test.streamalert.data',
                     'kms_key_arn': '${aws_kms_key.server_side_encryption.arn}',
                     'enable_alarm': True,
                     'evaluation_periods': 10,
                     'period_seconds': 3600,
                     'alarm_threshold': 100000,
                     'alarm_actions': [
-                        'arn:aws:sns:us-west-1:12345678910:unit-testing_streamalert_monitoring'
+                        'arn:aws:sns:us-west-1:12345678910:unit-test_streamalert_monitoring'
                     ]
                 }
             }
@@ -180,7 +180,7 @@ class TestFirehoseGenerate:
                     'compression_format': 'GZIP',
                     'log_name': 'json_embedded',
                     'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
-                    's3_bucket_name': 'unit-testing.streamalert.data',
+                    's3_bucket_name': 'unit-test.streamalert.data',
                     'kms_key_arn': '${aws_kms_key.server_side_encryption.arn}',
                     'enable_alarm': True,
                     'alarm_actions': ['do something crazy']

terraform/modules/tf_athena/variables.tf
@@ -48,8 +48,7 @@ variable "s3_logging_bucket" {
 }
 
 variable "database_name" {
-  type    = "string"
-  default = "streamalert"
+  type = "string"
 }
 
 variable "queue_name" {

terraform/modules/tf_classifier/variables.tf
@@ -6,6 +6,10 @@ variable "region" {
   type = "string"
 }
 
+variable "prefix" {
+  type = "string"
+}
+
 variable "function_role_id" {
   description = "Classifier function IAM Role ID, exported from the tf_lambda module"
 }

terraform/modules/tf_metric_filters/main.tf
@@ -5,8 +5,9 @@ resource "aws_cloudwatch_log_metric_filter" "cloudwatch_metric_filters" {
   log_group_name = "${var.log_group_name}"
 
   metric_transformation {
-    name  = "${var.metric_name}"
-    value = "${var.metric_value}"
+    name          = "${var.metric_name}"
+    value         = "${var.metric_value}"
+    default_value = "${var.metric_default_value}"
 
     namespace = "StreamAlert"
   }

terraform/modules/tf_metric_filters/variables.tf
@@ -4,12 +4,18 @@ variable "metric_name" {
 }
 
 variable "metric_value" {
-  description = "CloudWatch Log Group name to which the filter should be applied"
+  description = "The value that should be published to the metric"
   type        = "string"
 }
 
+variable "metric_default_value" {
+  description = "The value to emit when a filter pattern does not match a log event."
+  default     = 0
+}
+
+// See: https://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/FilterAndPatternSyntax.html
 variable "metric_pattern" {
-  description = "CloudWatch Log Group name to which the filter should be applied"
+  description = "A valid CloudWatch Logs filter pattern for extracting metric data out of ingested log events"
   type        = "string"
 }
 

commit 937c38ba2c87ab04bcb0711bd04d840d0566598c

fixing missing tf var and adding support for optional firehose prefixing (#1024)
* fixing bug introduced in #1022

* adding optional prefixing for firehose

* adding client support for optional prefix

* nit naming change

* updating old tf var for bug fixed upstream

* fixing resource name in lookuptables

* pr feedback

streamalert/classifier/clients/firehose.py
@@ -52,7 +52,7 @@ class FirehoseClient:
     MAX_RECORD_SIZE = 1000 * 1000 - 2
 
     # Default firehose name format, should be formatted with deployment prefix
-    DEFAULT_FIREHOSE_FMT = '{}_streamalert_data_{}'
+    DEFAULT_FIREHOSE_FMT = '{}streamalert_data_{}'
 
     # Exception for which backoff operations should be performed
     EXCEPTIONS_TO_BACKOFF = (ClientError, BotocoreConnectionError, HTTPClientError)
@@ -61,7 +61,11 @@ class FirehoseClient:
     _ENABLED_LOGS = dict()
 
     def __init__(self, prefix, firehose_config=None, log_sources=None):
-        self._prefix = prefix
+        self._prefix = (
+            '{}_'.format(prefix)
+            if firehose_config and firehose_config.get('use_prefix')
+            else ''
+        )
         self._client = boto3.client('firehose', config=boto_helpers.default_config())
         self.load_enabled_log_sources(firehose_config, log_sources, force_load=True)
 

streamalert_cli/terraform/firehose.py
@@ -65,6 +65,8 @@ def generate_firehose(logging_bucket, main_dict, config):
             'compression_format': (
                 config['global']['infrastructure']['firehose'].get('compression_format', 'GZIP')
             ),
+            'use_prefix': config['global']['infrastructure']['firehose'].get('use_prefix', True),
+            'prefix': config['global']['account']['prefix'],
             'log_name': log_stream_name,
             'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
             's3_bucket_name': firehose_s3_bucket_name,

terraform/modules/tf_kinesis_firehose_delivery_stream/main.tf
@@ -5,7 +5,7 @@
 //
 // This is a less destructive approach to creating all of the Streams.
 resource "aws_kinesis_firehose_delivery_stream" "streamalert_data" {
-  name        = "${var.prefix}_streamalert_data_${var.log_name}"
+  name        = "${var.use_prefix ? "${var.prefix}_" : ""}streamalert_data_${var.log_name}"
   destination = "s3"
 
   s3_configuration {

terraform/modules/tf_kinesis_firehose_delivery_stream/variables.tf
@@ -1,3 +1,11 @@
+variable "prefix" {
+  type = "string"
+}
+
+variable "use_prefix" {
+  description = "When true, prepends the StreamAlert prefix to the AWS Firehose resource name"
+}
+
 variable "buffer_size" {
   default = 5
 }

terraform/modules/tf_lambda/variables.tf
@@ -37,12 +37,8 @@ variable "filename" {
   description = "Path to .zip deployment package"
 }
 
-// NOTE: Due to https://github.com/terraform-providers/terraform-provider-aws/issues/3803,
-// 0 is interpreted as "disable reserve concurrency," and is the default value in Terraform.
-// (You can set reserve concurrency to 0 in the AWS console to stop all invocations.)
-// This could change in a future release of the AWS provider.
 variable "concurrency_limit" {
-  default     = 0
+  default     = -1
   description = "Optional reserved concurrency. By default, there is no function-specific concurrency limit."
 }
 

terraform/modules/tf_lookup_tables_policy/main.tf
@@ -3,7 +3,7 @@ resource "aws_iam_policy" "streamalert_read_from_lookup_tables" {
   policy = "${var.policy_json}"
 }
 
-resource "aws_iam_role_policy_attachment" "streamalert_read_from_lookup_tables_s3" {
+resource "aws_iam_role_policy_attachment" "streamalert_read_from_lookup_tables" {
   count      = "${var.role_count}"
   role       = "${element(var.roles, count.index)}"
   policy_arn = "${aws_iam_policy.streamalert_read_from_lookup_tables.arn}"

terraform/modules/tf_rules_engine/iam.tf
@@ -1,7 +1,7 @@
 // IAM Role Policy: Allow Rules Engine to read DynamoDB table (Threat Intel)
 resource "aws_iam_role_policy" "read_threat_intel_table" {
   count  = "${var.threat_intel_enabled ? 1 : 0}"
-  name   = "ReadDynamoDB"
+  name   = "ReadThreatIntelDynamoDB"
   role   = "${var.function_role_id}"
   policy = "${data.aws_iam_policy_document.read_threat_intel_table.json}"
 }
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "read_threat_intel_table" {
 // Allow the Rules Engine to read the rules table
 resource "aws_iam_role_policy" "read_rules_table" {
   count  = "${var.rules_table_arn == "" ? 0 : 1}"
-  name   = "ReadRulesTable"
+  name   = "ReadRulesDynamoDB"
   role   = "${var.function_role_id}"
   policy = "${data.aws_iam_policy_document.read_rules_table.json}"
 }
@@ -81,7 +81,7 @@ resource "aws_iam_role_policy" "rules_engine_policy" {
 
 // IAM Role Policy: Allow the Rules Engine to save alerts to dynamo.
 resource "aws_iam_role_policy" "save_alerts_to_dynamo" {
-  name   = "SaveAlertsToDynamo"
+  name   = "WriteAlertsDynamoDB"
   role   = "${var.function_role_id}"
   policy = "${data.aws_iam_policy_document.save_alerts_to_dynamo.json}"
 }

tests/unit/streamalert/classifier/clients/test_firehose.py
@@ -27,7 +27,10 @@ class TestFirehoseClient:
     def setup(self):
         """Setup before each method"""
         with patch('boto3.client'):  # patch to speed up unit tests slightly
-            self._client = FirehoseClient(prefix='unit-test')
+            self._client = FirehoseClient(
+                prefix='unit-test',
+                firehose_config={'use_prefix': True}
+            )
 
     def teardown(self):
         """Teardown after each method"""
@@ -410,4 +413,26 @@ class TestFirehoseClient:
         self._client.send(self._sample_payloads)
         send_batch_mock.assert_called_with(
             'unit-test_streamalert_data_log_type_01_sub_type_01', expected_batch
+        )
+
+    @patch.object(FirehoseClient, '_send_batch')
+    def test_send_no_prefixing(self, send_batch_mock):
+        """FirehoseClient - Send, No Prefixing"""
+        FirehoseClient._ENABLED_LOGS = {
+            'log_type_01_sub_type_01': 'log_type_01:sub_type_01'
+        }
+        expected_batch = [
+            '{"unit_key_01":1,"unit_key_02":"test"}\n',
+            '{"unit_key_01":2,"unit_key_02":"test"}\n'
+        ]
+
+        client = FirehoseClient.load_from_config(
+            prefix='unit-test',
+            firehose_config={'enabled': True, 'use_prefix': False},
+            log_sources=None
+        )
+
+        client.send(self._sample_payloads)
+        send_batch_mock.assert_called_with(
+            'streamalert_data_log_type_01_sub_type_01', expected_batch
         )

tests/unit/streamalert_cli/terraform/test_firehose.py
@@ -70,6 +70,8 @@ class TestFirehoseGenerate:
                     'buffer_size': 128,
                     'buffer_interval': 900,
                     'compression_format': 'GZIP',
+                    'use_prefix': True,
+                    'prefix': 'unit-test',
                     'log_name': 'json_embedded',
                     'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
                     's3_bucket_name': 'unit-test.streamalert.data',
@@ -101,6 +103,8 @@ class TestFirehoseGenerate:
                     'buffer_size': 128,
                     'buffer_interval': 900,
                     'compression_format': 'GZIP',
+                    'use_prefix': True,
+                    'prefix': 'unit-test',
                     'log_name': 'json_embedded',
                     'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
                     's3_bucket_name': 'unit-test.streamalert.data',
@@ -139,6 +143,8 @@ class TestFirehoseGenerate:
                     'buffer_size': 128,
                     'buffer_interval': 900,
                     'compression_format': 'GZIP',
+                    'use_prefix': True,
+                    'prefix': 'unit-test',
                     'log_name': 'json_embedded',
                     'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
                     's3_bucket_name': 'unit-test.streamalert.data',
@@ -178,6 +184,8 @@ class TestFirehoseGenerate:
                     'buffer_size': 128,
                     'buffer_interval': 900,
                     'compression_format': 'GZIP',
+                    'use_prefix': True,
+                    'prefix': 'unit-test',
                     'log_name': 'json_embedded',
                     'role_arn': '${module.kinesis_firehose_setup.firehose_role_arn}',
                     's3_bucket_name': 'unit-test.streamalert.data',