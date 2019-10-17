4f00b90d641a6efcd369e7459f6287c2f5a198c5
Ryan Deivert <ryan.deivert@airbnb.com>
2019-10-17 13:28:52 -0700

    adding aliased terraform providers for regions

---
+++ terraform/providers.tf
@@ -0,0 +1,84 @@
+// This root terraform file provides aliased providers for all known AWS regions
+// These should be utilized in all submodules, and a submodule should NEVER
+// define its own provider block.
+// See here for additional reading:
+//   https://www.terraform.io/docs/configuration/modules.html#providers-within-modules
+provider "aws" {
+  alias  = "ap-northeast-1"
+  region = "ap-northeast-1"
+}
+
+provider "aws" {
+  alias  = "ap-northeast-2"
+  region = "ap-northeast-2"
+}
+
+provider "aws" {
+  alias  = "ap-south-1"
+  region = "ap-south-1"
+}
+
+provider "aws" {
+  alias  = "ap-southeast-1"
+  region = "ap-southeast-1"
+}
+
+provider "aws" {
+  alias  = "ap-southeast-2"
+  region = "ap-southeast-2"
+}
+
+provider "aws" {
+  alias  = "ca-central-1"
+  region = "ca-central-1"
+}
+
+provider "aws" {
+  alias  = "eu-central-1"
+  region = "eu-central-1"
+}
+
+provider "aws" {
+  alias  = "eu-north-1"
+  region = "eu-north-1"
+}
+
+provider "aws" {
+  alias  = "eu-west-1"
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  alias  = "eu-west-2"
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "eu-west-3"
+  region = "eu-west-3"
+}
+
+provider "aws" {
+  alias  = "sa-east-1"
+  region = "sa-east-1"
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "us-east-2"
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias  = "us-west-1"
+  region = "us-west-1"
+}
+
+provider "aws" {
+  alias  = "us-west-2"
+  region = "us-west-2"
+}
