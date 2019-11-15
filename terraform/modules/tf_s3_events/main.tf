a77317f210264e5b906a6675d3a4af1f72b37f03
Ryan Deivert <ryan.deivert@airbnb.com>
2019-11-15 14:12:28 -0800

    updating tf_s3_events module for proper handling of more than one filter per bucket

--- terraform/modules/tf_s3_events/main.tf
+++ terraform/modules/tf_s3_events/main.tf
@@ -1,30 +1,32 @@
 // Lambda Permission: Allow S3 Event Notifications to invoke Lambda
 resource "aws_lambda_permission" "allow_bucket" {
-  statement_id  = "InvokeFromS3Bucket_${var.notification_id}"
+  statement_id  = "${var.prefix}_${var.cluster}_InvokeFromS3Bucket_${var.bucket_name}"
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::${var.bucket_id}"
+  source_arn    = "arn:aws:s3:::${var.bucket_name}"
   qualifier     = var.lambda_function_alias
 }
 
 // S3 Bucket Notification: Invoke the StreamAlert Classifier
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  count  = var.enable_events ? 1 : 0
-  bucket = var.bucket_id
+  bucket = var.bucket_name
 
-  lambda_function {
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = var.filter_prefix
-    filter_suffix       = var.filter_suffix
-    id                  = "notify_${var.notification_id}"
-    lambda_function_arn = var.lambda_function_alias_arn
+  dynamic "lambda_function" {
+    for_each = var.filters
+
+    content {
+      events              = ["s3:ObjectCreated:*"]
+      filter_prefix       = lookup(lambda_function.value, "filter_prefix", "") // use lookup since this is optional
+      filter_suffix       = lookup(lambda_function.value, "filter_suffix", "") // use lookup since this is optional
+      lambda_function_arn = var.lambda_function_alias_arn
+    }
   }
 }
 
 // IAM Policy: Allow Lambda to GetObjects from S3
 resource "aws_iam_role_policy" "lambda_s3_permission" {
-  name   = "S3GetObjects_${var.notification_id}"
+  name   = "${var.prefix}_${var.cluster}_S3GetObjects_${var.bucket_name}"
   role   = var.lambda_role_id
   policy = data.aws_iam_policy_document.s3_read_only.json
 }
@@ -39,7 +41,7 @@ data "aws_iam_policy_document" "s3_read_only" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.bucket_id}",
+      "arn:aws:s3:::${var.bucket_name}",
     ]
   }
 
@@ -51,7 +53,7 @@ data "aws_iam_policy_document" "s3_read_only" {
     ]
 
     resources = [
-      "arn:aws:s3:::${var.bucket_id}/*",
+      "arn:aws:s3:::${var.bucket_name}/*",
     ]
   }
 }
