c1c0a731793067e1844e794ce2feadd7d81e0779
Ryan Deivert <ryan.deivert@airbnb.com>
2019-12-11 13:21:08 -0800

    updating s3_events tf module for flexibility

--- tests/unit/streamalert_cli/terraform/test_s3_events.py
+++ tests/unit/streamalert_cli/terraform/test_s3_events.py
@@ -22,7 +22,7 @@ CONFIG = CLIConfig(config_path='tests/unit/conf')
 
 
 def test_generate_s3_events():
-    """CLI - Terraform - S3 Events"""
+    """CLI - Terraform - S3 Events, No Module Prefix"""
     cluster_dict = common.infinitedict()
     result = s3_events.generate_s3_events('advanced', cluster_dict, CONFIG)
 
@@ -59,4 +59,45 @@ def test_generate_s3_events():
     }
 
     assert_equal(result, True)
+    assert_equal(cluster_dict, expected_config)
+
+
+def test_generate_s3_events_with_prefix():
+    """CLI - Terraform - S3 Events, With Module Prefix"""
+    cluster_dict = common.infinitedict()
+    bucket_config = {
+        'unit-test-bucket': [
+            {
+                'filter_prefix': 'AWSLogs/123456789/CloudTrail/us-east-1/',
+            }
+        ]
+    }
+    s3_events.generate_s3_events_by_bucket(
+        'advanced',
+        cluster_dict,
+        CONFIG,
+        bucket_config,
+        module_prefix='cloudtrail'
+    )
+
+    expected_config = {
+        'module': {
+            'cloudtrail_s3_events_unit-test_advanced_unit-test-bucket': {
+                'source': './modules/tf_s3_events',
+                'lambda_function_alias': '${module.classifier_advanced_lambda.function_alias}',
+                'lambda_function_alias_arn': (
+                    '${module.classifier_advanced_lambda.function_alias_arn}'
+                ),
+                'lambda_function_name': '${module.classifier_advanced_lambda.function_name}',
+                'bucket_name': 'unit-test-bucket',
+                'lambda_role_id': '${module.classifier_advanced_lambda.role_id}',
+                'filters': [
+                    {
+                        'filter_prefix': 'AWSLogs/123456789/CloudTrail/us-east-1/'
+                    }
+                ]
+            },
+        }
+    }
+
     assert_equal(cluster_dict, expected_config)
