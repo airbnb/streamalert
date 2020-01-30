fbbc57ec355fc3a5205e180c170e34814e90815a
Garret Reece <GarretReece@users.noreply.github.com>
2020-01-30 10:57:07 -0600

    threat_intel_downloader module now uses tf_lambda module (#1074)
    * threat intel downloader terraform module now uses tf_lambda
    
    * small cleanups make for happier linters
    
    * fixed some stale references in the threat_intel_downloader terraform module

--- terraform/modules/tf_threat_intel_downloader/iam.tf
+++ terraform/modules/tf_threat_intel_downloader/iam.tf
@@ -1,31 +1,7 @@
-// IAM Role: Execution Role
-resource "aws_iam_role" "threat_intel_downloader" {
-  name               = "${var.prefix}_threat_intel_downloader"
-  path               = "/streamalert/"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
-
-  tags = {
-    Name = "StreamAlert"
-  }
-}
-
-// IAM Policy Doc: Generic Lambda AssumeRole
-data "aws_iam_policy_document" "lambda_assume_role_policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
 // IAM Role Policy: Allow lambda function to invoke the Lambda Function
 resource "aws_iam_role_policy" "threat_intel_downloader" {
   name   = "InvokeLambda"
-  role   = aws_iam_role.threat_intel_downloader.id
+  role   = var.function_role_id
   policy = data.aws_iam_policy_document.invoke_lambda_function.json
 }
 
@@ -39,7 +15,7 @@ data "aws_iam_policy_document" "invoke_lambda_function" {
     ]
 
     resources = [
-      aws_lambda_function.threat_intel_downloader.arn,
+      var.function_alias_arn
     ]
   }
 }
@@ -47,7 +23,7 @@ data "aws_iam_policy_document" "invoke_lambda_function" {
 // IAM Role Policy: Allow the lambda function to create/update CloudWatch logs
 resource "aws_iam_role_policy" "cloudwatch_logs" {
   name   = "WriteToCloudwatchLogs"
-  role   = aws_iam_role.threat_intel_downloader.id
+  role   = var.function_role_id
   policy = data.aws_iam_policy_document.cloudwatch_logs_policy.json
 }
 
@@ -61,7 +37,7 @@ data "aws_iam_policy_document" "cloudwatch_logs_policy" {
     ]
 
     resources = [
-      aws_cloudwatch_log_group.threat_intel_downloader.arn,
+      var.function_cloudwatch_log_group_name
     ]
   }
 
@@ -74,7 +50,7 @@ data "aws_iam_policy_document" "cloudwatch_logs_policy" {
     ]
 
     resources = [
-      "arn:aws:logs:${var.region}:${var.account_id}:log-group:${aws_cloudwatch_log_group.threat_intel_downloader.name}:log-stream:*",
+      "arn:aws:logs:${var.region}:${var.account_id}:log-group:${var.function_cloudwatch_log_group_name}:log-stream:*",
     ]
   }
 }
@@ -82,7 +58,7 @@ data "aws_iam_policy_document" "cloudwatch_logs_policy" {
 // IAM role policy: Allow lambda function to read/write data from DynamoDB
 resource "aws_iam_role_policy" "read_write_dynamodb" {
   name   = "ReadDynamoDB"
-  role   = aws_iam_role.threat_intel_downloader.id
+  role   = var.function_role_id
   policy = data.aws_iam_policy_document.read_write_dynamodb.json
 }
 
@@ -107,7 +83,7 @@ data "aws_iam_policy_document" "read_write_dynamodb" {
 // IAM role policy: Allow lambda function to read from parameter store
 resource "aws_iam_role_policy" "get_api_creds_from_ssm" {
   name   = "GetSSMParams"
-  role   = aws_iam_role.threat_intel_downloader.id
+  role   = var.function_role_id
   policy = data.aws_iam_policy_document.get_api_creds_from_ssm.json
 }
 
@@ -129,7 +105,7 @@ data "aws_iam_policy_document" "get_api_creds_from_ssm" {
 // IAM Role Policy: Allow the Threat Intel Downloader function to publish sns (used for DLQ)
 resource "aws_iam_role_policy" "theat_intel_downloader_publish_sns" {
   name   = "PublishToSNS"
-  role   = aws_iam_role.threat_intel_downloader.id
+  role   = var.function_role_id
   policy = data.aws_iam_policy_document.theat_intel_downloader_publish_sns.json
 }
 
