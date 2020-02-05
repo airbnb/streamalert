2bec070f971d6c6e9e3063525399037e5d56837b
jack1902 <39212456+jack1902@users.noreply.github.com>
2020-02-05 10:29:31 +0000

    [setup] Natively pass AWS credentials through ssh
    Updated the sshd_config file to allow AWS_* to be passed through when
    using vagrant ssh. This allows you to not hardcode credentials inside of
    the vm
    
    Signed-off-by: jack1902 <39212456+jack1902@users.noreply.github.com>

--- vagrant/cli/streamalert/install.sh
+++ vagrant/cli/streamalert/install.sh
@@ -1,2 +1,7 @@
 # Install git so we can clone the streamalert repository
 apt-get install git awscli -y
+
+# Configure ssh to allow env variables AWS_* to be passed through
+cp /vagrant/vagrant/cli/streamalert/sshd_config /etc/ssh/sshd_config && \
+  /etc/init.d/ssh restart
+
