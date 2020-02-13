64931601324bc3954522ebc711a66228ea5fe730
darkjokelady <chunyong.lin@gmail.com>
2020-02-13 15:51:25 -0800

    [rules] Add community rule to detect ssh login activity based on osquery events (#1127)
    * [rules] Add community rule to alert on ssh login activity based on osquery detection
    
    * address comments

--- rules/matchers/matchers.py
+++ rules/matchers/matchers.py
@@ -14,7 +14,40 @@ You can also supply multiple matchers for many common scenarios:
 @rule('root_logins', logs=['osquery:differential'],
       matchers=[matchers.prod, matchers.pci], outputs=['pagerduty:sample-integration'])
 """
+class GuardDutyMatcher:
+    """A class contains matchers for AWS GuardDuty service"""
+
+    @classmethod
+    def guard_duty(cls, rec):
+        return rec['detail-type'] == 'GuardDuty Finding'
+
+class OsqueryMatcher:
+    """A class defines contains matchers for Osquery events"""
+
+    _EVENT_TYPE_LOGIN = 7
+    _RUNLEVELS = {
+        '',
+        'LOGIN',
+        'reboot',
+        'shutdown',
+        'runlevel'
+    }
 
 
-def guard_duty(record):
-    return record['detail-type'] == 'GuardDuty Finding'
+    @classmethod
+    def added(cls, rec):
+        return rec['action'] == 'added'
+
+
+    @classmethod
+    def user_login(cls, rec):
+        """Capture user logins from the osquery last table
+        This matcher assumes we use default osquery pack shipped with osquery package
+        located at /usr/share/osquery/packs/incident-response.conf on the linux host.
+        Update the pack name (rec['name']) if it is different.
+        """
+        return (
+            rec['name'] == 'pack_incident-response_last' and
+            int(rec['columns']['type']) == cls._EVENT_TYPE_LOGIN and
+            (rec['columns']['username'] not in cls._RUNLEVELS)
+        )
