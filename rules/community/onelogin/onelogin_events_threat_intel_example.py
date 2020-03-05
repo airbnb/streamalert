469bfdf484cac03321058f03e6e0a597acdfbc91
Ryan Deivert <ryan.deivert@airbnb.com>
2020-03-05 11:00:09 -0800

    prettify and cleanup

--- rules/community/onelogin/onelogin_events_threat_intel_example.py
+++ rules/community/onelogin/onelogin_events_threat_intel_example.py
@@ -6,7 +6,7 @@ from streamalert.rules_engine.threat_intel import ThreatIntel
 @rule(logs=['onelogin:events'])
 def onelogin_events_threat_intel_example(rec):
     """
-    description:  Alert on OneLogin activity from a malicious IP address using threat intel
+    description: Alert on OneLogin activity from a malicious IP address using threat intel
     note: This is purely for example purposes in testing, and is not meant to be used as-is
     """
     return ThreatIntel.IOC_KEY in rec and 'ip' in rec[ThreatIntel.IOC_KEY]
