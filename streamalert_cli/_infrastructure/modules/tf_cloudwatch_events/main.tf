71cae1e6bb7c1e04345ff9a9d78e70bc0ef8b397
Ryan Deivert <ryan.deivert@airbnb.com>
2020-07-27 12:45:17 -0700

    updating cloudwatch events module to support advanced event brige rule

--- streamalert_cli/_infrastructure/modules/tf_cloudwatch_events/main.tf
+++ streamalert_cli/_infrastructure/modules/tf_cloudwatch_events/main.tf
@@ -1,22 +1,3 @@
-// Cloudwatch event to capture Cloudtrail API calls
-resource "aws_cloudwatch_event_rule" "capture_events" {
-  name          = "${var.prefix}_${var.cluster}_streamalert_all_events"
-  description   = "Capture CloudWatch events"
-  role_arn      = aws_iam_role.cloudwatch_events_role.arn
-  event_pattern = var.event_pattern
-
-  tags = {
-    Name    = "StreamAlert"
-    Cluster = var.cluster
-  }
-}
-
-// The Kinesis destination for Cloudwatch events
-resource "aws_cloudwatch_event_target" "kinesis" {
-  rule = aws_cloudwatch_event_rule.capture_events.name
-  arn  = var.kinesis_arn
-}
-
 // IAM Role: CloudWatch Events
 resource "aws_iam_role" "cloudwatch_events_role" {
   name               = "${var.prefix}_${var.cluster}_cloudwatch_events_role"
@@ -62,3 +43,73 @@ data "aws_iam_policy_document" "kinesis_put_records" {
     ]
   }
 }
+
+
+// CloudFormation Stack to substitute for this limitation in Terraform:
+//   https://github.com/terraform-providers/terraform-provider-aws/issues/8759
+//
+// This stack creates a rule in EventBridge (vs CloudWatch Events) since Terraform currently
+// does not support apply this directly. EventBridge is needed for this rule, because content-based
+// filtering with Event Patterns is not a supported feature in the "legacy" aws_cloudwatch_event_rule
+// See here:
+//   https://docs.aws.amazon.com/eventbridge/latest/userguide/content-filtering-with-event-patterns.html#filtering-anything-but-matching-example
+//
+// The aws_cloudwatch_event_rule event patterns only support strings, or list of strings, vs the more
+// advanced mapping with things like "anything-but". Once this is supported by Terraform, this stack
+// can be removed and the below code uncommented to be applied.
+resource "aws_cloudformation_stack" "eventbridge_rule" {
+  name = "CloudWatchEvents${var.prefix}${var.cluster}StreamAlert"
+
+  // Ideally, the AWS::Events::Rule resource would also be tagged here. However, it appears that
+  // while the resource itself supports tagging, CloudFormation does not in fact support it for
+  // this resource type...
+  template_body = <<EOF
+Resources:
+  ${var.prefix}${var.cluster}StreamAlertEventBridgeRule:
+    Type: AWS::Events::Rule
+    Properties:
+      Name: "${var.prefix}_${var.cluster}_streamalert_all_events"
+      Description: "Capture CloudWatch events"
+      EventPattern: "${var.event_pattern}"
+      RoleArn: "${aws_iam_role.cloudwatch_events_role.arn}"
+      Targets:
+        - Arn: "${var.kinesis_arn}"
+          Id: "${var.prefix}_${var.cluster}_streamalert_kinesis"
+          RoleArn: "${aws_iam_role.cloudwatch_events_role.arn}"
+EOF
+
+  tags = {
+    Name    = "StreamAlert"
+    Cluster = var.cluster
+  }
+}
+
+// Uncomment the below when it is properly supported by Terraform, and remove the above stack
+########################################################
+########################################################
+################# KEEP FOR FUTURE USE ##################
+########################################################
+########################################################
+// Cloudwatch Event Rule: Capture CloudWatch Events
+# resource "aws_cloudwatch_event_rule" "capture_events" {
+#   name          = "${var.prefix}_${var.cluster}_streamalert_all_events"
+#   description   = "Capture CloudWatch events"
+#   role_arn      = aws_iam_role.cloudwatch_events_role.arn
+#   event_pattern = var.event_pattern
+#
+#   tags = {
+#     Name    = "StreamAlert"
+#     Cluster = var.cluster
+#   }
+# }
+#
+// The Kinesis destination for Cloudwatch events
+# resource "aws_cloudwatch_event_target" "kinesis" {
+#   rule = aws_cloudwatch_event_rule.capture_events.name
+#   arn  = var.kinesis_arn
+# }
+########################################################
+########################################################
+################# KEEP FOR FUTURE USE ##################
+########################################################
+########################################################
