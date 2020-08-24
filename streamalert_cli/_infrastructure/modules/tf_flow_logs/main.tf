461ef2500c1d20c0f705730629379cb6201acb33
Ryan Deivert <ryan.deivert@airbnb.com>
2020-08-24 13:30:33 -0700

    updating for vpc flow logs

--- streamalert_cli/_infrastructure/modules/tf_flow_logs/main.tf
+++ streamalert_cli/_infrastructure/modules/tf_flow_logs/main.tf
@@ -1,25 +1,28 @@
 resource "aws_flow_log" "vpc_flow_log" {
-  count          = length(var.vpcs)
-  vpc_id         = element(var.vpcs, count.index)
-  log_group_name = aws_cloudwatch_log_group.flow_log_group.name
-  iam_role_arn   = aws_iam_role.flow_log_role.arn
-  traffic_type   = "ALL"
+  count                = length(var.vpcs)
+  vpc_id               = element(var.vpcs, count.index)
+  log_destination      = aws_cloudwatch_log_group.flow_log_group.arn
+  log_destination_type = "cloud-watch-logs"
+  iam_role_arn         = aws_iam_role.flow_log_role.arn
+  traffic_type         = "ALL"
 }
 
 resource "aws_flow_log" "subnet_flow_log" {
-  count          = length(var.subnets)
-  subnet_id      = element(var.subnets, count.index)
-  log_group_name = aws_cloudwatch_log_group.flow_log_group.name
-  iam_role_arn   = aws_iam_role.flow_log_role.arn
-  traffic_type   = "ALL"
+  count                = length(var.subnets)
+  subnet_id            = element(var.subnets, count.index)
+  log_destination      = aws_cloudwatch_log_group.flow_log_group.arn
+  log_destination_type = "cloud-watch-logs"
+  iam_role_arn         = aws_iam_role.flow_log_role.arn
+  traffic_type         = "ALL"
 }
 
 resource "aws_flow_log" "eni_flow_log" {
-  count          = length(var.enis)
-  eni_id         = element(var.enis, count.index)
-  log_group_name = aws_cloudwatch_log_group.flow_log_group.name
-  iam_role_arn   = aws_iam_role.flow_log_role.arn
-  traffic_type   = "ALL"
+  count                = length(var.enis)
+  eni_id               = element(var.enis, count.index)
+  log_destination      = aws_cloudwatch_log_group.flow_log_group.arn
+  log_destination_type = "cloud-watch-logs"
+  iam_role_arn         = aws_iam_role.flow_log_role.arn
+  traffic_type         = "ALL"
 }
 
 resource "aws_cloudwatch_log_group" "flow_log_group" {
@@ -34,8 +37,7 @@ resource "aws_cloudwatch_log_group" "flow_log_group" {
 
 resource "aws_cloudwatch_log_subscription_filter" "flow_logs" {
   name            = "${aws_cloudwatch_log_group.flow_log_group.name}_to_kinesis"
-  log_group_name  = "${aws_cloudwatch_log_group.flow_log_group.name}"
-  filter_pattern  = "${var.flow_log_filter}"
-  destination_arn = "${var.cloudwatch_logs_destination_arn}"
+  log_group_name  = aws_cloudwatch_log_group.flow_log_group.name
+  filter_pattern  = var.flow_log_filter
+  destination_arn = var.cloudwatch_logs_destination_arn
 }
-
